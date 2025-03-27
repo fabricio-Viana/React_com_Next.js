35b906566b561538ad07cc1f82c11b690f619ea3
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-03-27 00:18:34 -0300

    componentes-02

--- app/basico/08_componentes/page.jsx
+++ app/basico/08_componentes/page.jsx
@@ -2,6 +2,19 @@ import Componente from "@/app/componentes/Componente-01"
 
 export default function componente (){
     return <>
-        <div> <Componente/> </div>
+        <div> 
+            <Componente
+                principal="Este é o titulo"
+                secundario="Este é o subtitulo"
+                pequeno 
+            />
+        </div>
+        <div> 
+            <Componente
+                principal="login"
+                secundario="senha"
+                pequeno = {false}
+            />
+        </div>
     </>
 }
--- app/componentes/Componente-01.jsx
+++ app/componentes/Componente-01.jsx
@@ -1,6 +1,12 @@
-export default function Componente(){
-    return<>
-        <h1>Titulo</h1>
-        <h2>subtitulo</h2>
+export default function Componente(props){
+    console.log(props)
+    return props.pequeno?<>
+        <h1>{props.principal}</h1>
+        <h2>{props.secundario}</h2>
+    </>
+    :
+    <>
+        <p>{props.principal}</p>
+        <p>{props.secundario}</p>
     </>
 }
