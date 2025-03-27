c089e7893425d9414537536ddb5bda71989d36bf
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-03-26 23:06:37 -0300

    Fragente-01

---
+++ app/basico/06_fragment/page.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function fragment (){
+    return (
+        <React.Fragment>
+            <h1>test</h1>
+            <h2>test 2</h2>
+        </React.Fragment>
+    )
+}
